5764cce6b85dde2be364369ddccb169c3fd5791e
Marnie Robinson <marnielane@gmail.com>
2021-01-09 20:46:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ introduction("Edwin")
 def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Edwin", "Ruby")
+introduction_with_language("Maria", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
    "Hi, my name is #{name} and I am learning to program in #{language}."
